bbbb8b271c6d31b0e727cd214c518ee2b7b9858e
CassioJhones <cassio.bjhones@gmail.com>
2024-04-28 21:37:29 -0300

    Adicionar arquivos de projeto.

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.Register;
+using PassIn.Communication.Requests;
+
+namespace PassIn.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestEventJson request)
+    {
+		try
+		{
+            RegisterEventUseCase useCase = new RegisterEventUseCase();
+			useCase.Execute(request);
+
+			return Ok();
+		}
+		catch (ArgumentException erro)
+		{
+            return BadRequest($"{erro.Message}");
+        }
+    }
+}
---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,25 @@
+﻿using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Events.Register;
+
+public class RegisterEventUseCase
+{
+    public void Execute(RequestEventJson request)
+    {
+
+        Validate(request);
+
+    }
+
+    public void Validate(RequestEventJson request)
+    {
+        if (request.MaximumAttendees <= 0)
+            throw new ArgumentException("Numero maximo invalido");
+
+        if (string.IsNullOrWhiteSpace(request.Title))
+            throw new ArgumentException("Titulo invalido");
+
+        if (string.IsNullOrWhiteSpace(request.Details))
+            throw new ArgumentException("Detalhes invalidos");
+    }
+}
