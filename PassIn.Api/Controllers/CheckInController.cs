d4491543c15fa629816f25dfe14b5b7fd3434ebe
CassioJhones <cassio.bjhones@gmail.com>
2024-05-16 04:11:03 -0300

    NovaEntidade, NovoController

---
+++ PassIn.Api/Controllers/CheckInController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.CheckIns.MakeCheckIn;
+using PassIn.Communication.Responses;
+
+namespace PassIn.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CheckInController : ControllerBase
+{
+    [HttpPost]
+    [Route("{atendeId}")]
+    [ProducesResponseType(typeof(ResponseRegisteredJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status409Conflict)]
+    public IActionResult CheckIn([FromRoute] Guid atendeId)
+    {
+        var useCase = new MakeCheckinUseCase();
+        var teste = useCase.Execute(atendeId);
+        return Created();
+    }
+}
---
+++ PassIn.Application/UseCases/CheckIns/MakeCheckIn/MakeCheckinUseCase.cs
@@ -0,0 +1,41 @@
+﻿using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+using PassIn.Infrastructure.Entities;
+
+namespace PassIn.Application.UseCases.CheckIns.MakeCheckIn;
+public class MakeCheckinUseCase
+{
+    private readonly PassInDbContext _bancoSQL;
+    public MakeCheckinUseCase()
+        => _bancoSQL = new PassInDbContext();
+
+    public ResponseRegisteredJson Execute(Guid atendeId)
+    {
+        Validate(atendeId);
+
+        CheckIn entidade = new()
+        {
+            Attendee_Id = atendeId,
+            Created_at = DateTime.UtcNow,
+        };
+        _bancoSQL.CheckIns.Add(entidade);
+        _bancoSQL.SaveChanges();
+
+        return new ResponseRegisteredJson
+        {
+            Id = entidade.Id,
+        };
+    }
+
+    private void Validate(Guid atendeId)
+    {
+        bool existePessoa = _bancoSQL.Attendees.Any(p => p.Id == atendeId);
+        if (!existePessoa)
+            throw new NotFoundException("Nao encontrado");
+
+        bool existChekin = _bancoSQL.CheckIns.Any(check => check.Attendee_Id == atendeId);
+        if (existePessoa)
+            throw new ConflictException("Já Cadastrado neste evento");
+    }
+}
