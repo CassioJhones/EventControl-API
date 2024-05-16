e7f6fdef388b340f3b059dddd8c9e87d06e3803b
CassioJhones <cassio.bjhones@gmail.com>
2024-05-16 03:45:55 -0300

    correco quantidade

--- PassIn.Application/UseCases/Events/GetById/GetEventByIdUseCase.cs
+++ PassIn.Application/UseCases/Events/GetById/GetEventByIdUseCase.cs
@@ -1,4 +1,5 @@
-﻿using PassIn.Communication.Responses;
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Communication.Responses;
 using PassIn.Exceptions;
 using PassIn.Infrastructure;
 using PassIn.Infrastructure.Entities;
@@ -9,18 +10,20 @@ public class GetEventByIdUseCase
 
     public ResponseEventJson Execute(Guid id)
     {
-        PassInDbContext bancoSQLite = new PassInDbContext();
-        Event? evento = bancoSQLite.Events.Find(id);
+        PassInDbContext bancoSQLite = new();
+        Event? evento = bancoSQLite.Events
+            .Include(ev => ev.Attendees)
+            .FirstOrDefault(ev => ev.Id == id);
         if (evento is null)
             throw new NotFoundException("Id nao existe");
 
         return new ResponseEventJson
         {
             Id = evento.Id,
-            Title   = evento.Title,
-            Details  = evento.Details,
+            Title = evento.Title,
+            Details = evento.Details,
             MaximumAttendees = evento.Maximum_Attendees,
-            AttendeesAmount = -1
+            AttendeesAmount = evento.Attendees.Count(),
         };
     }
 }
