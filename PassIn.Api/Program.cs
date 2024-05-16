9b2b18e0635b48cee435e849089e0f57bcd34fe1
CassioJhones <cassio.bjhones@gmail.com>
2024-05-16 16:49:58 -0300

    .

--- PassIn.Api/Program.cs
+++ PassIn.Api/Program.cs
@@ -5,6 +5,8 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
 WebApplication app = builder.Build();
